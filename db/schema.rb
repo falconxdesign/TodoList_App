552602a2a5e2821dee0e7cb39e1a4b6afdd7f397
falconxdesign <bladefreaky@gmail.com>
2020-04-18 05:05:32 +0000

    Added todo items and delete functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_18_034653) do
+ActiveRecord::Schema.define(version: 2020_04_18_040320) do
+
+  create_table "todo_items", force: :cascade do |t|
+    t.string "content"
+    t.integer "todo_list_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["todo_list_id"], name: "index_todo_items_on_todo_list_id"
+  end
 
   create_table "todo_lists", force: :cascade do |t|
     t.string "title"
@@ -19,4 +27,5 @@ ActiveRecord::Schema.define(version: 2020_04_18_034653) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "todo_items", "todo_lists"
 end
